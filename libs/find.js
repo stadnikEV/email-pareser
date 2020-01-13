2d5c703d3d1ea85bd04bdbcc5f6128b5415bb03f
StadnikEV <stadnik24fps@gmail.com>
2020-01-13 17:17:47 +0200

    fix: statistic

--- libs/find.js
+++ libs/find.js
@@ -3,34 +3,10 @@ const Block = require('./block')
 const consoleProgress = require('./console-progress')
 
 
-
-// const addToStatistic = ({ statistic, message, id, link, email }) => {
-//   if (!statistic[message]) {
-//     statistic[message] = []
-//   }
-//
-//   if (email) {
-//     statistic.email += 1
-//   }
-//
-//   statistic.all += 1
-//
-//   const companyData = { id }
-//
-//   if (link) {
-//     companyData.link = link
-//   }
-//
-//   statistic[message].push(companyData)
-//
-//   return statistic
-// }
-
 module.exports = companies => {
   return new Promise(async function(resolve, reject) {
     const block = new Block()
     const result = []
-    // let statistic = { email: 0, all: 0}
     let company = null
 
     const lastId = companies[0].id
@@ -56,7 +32,6 @@ module.exports = companies => {
         consoleProgress({ message: response.status, email: response.email, id: company.id, found, lastId, firstId })
 
         result.push(company)
-        // statistic = addToStatistic({ statistic, message: response.status, id: company.id, link: response.link, email: response.email  })
 
         setTimeout(() => {
           find()
@@ -72,13 +47,10 @@ module.exports = companies => {
           company.link = e.link
           result.push(company)
 
-          // statistic = addToStatistic({ statistic, message: e.errorMessage, id: company.id, link: e.link  })
-
           if (block.isBlock({ message: e.errorMessage })) {
             console.log('Блокировка запросов')
             const limit = -block.getLimit()
             result.splice(limit)
-            statistic.all -= limit
             resolve({ searchResult: result })
             return
           }
